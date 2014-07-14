ec061520c230a15a53973c0d6efd1ffb2354260e
cherish.peng <cherish.peng@vipshop.com>
2014-07-14 02:52:52 -0700

    Update

--- jquery.fn.outerhtml.js
+++ jquery.fn.outerhtml.js
@@ -1,28 +1,18 @@
 (function (global, factory) {
-    'use strict';
     // Node.js, CommonJS, CommonJS Like
     if (typeof module === 'object' && typeof module.exports === 'object') {
         module.exports = factory(global, true);
     } else {
         factory(global);
     }
-})(this, function (global, noGlobal) {
+})(this, function(global, noGlobal) {
     // Support cmd && amd
     if (define && (define.cmd || define.amd)) {
-        return define("jquery.fn.outerhtml", [], factory);
-    }
-    // Global require
-    if (typeof require === 'function') {
-        return factory(require);
+        return define("jquery.fn.outerhtml", factory);
     }
     // Common
-    return factory(function (id) {
-        var key;
-        for (key in global)
-            if (key.toLowerCase() === id.toLowerCase())
-                return global[key];
-    });
-
+    return factory(require);
+	
     function factory(require, exports) {
         var
         $ = require('jquery');
